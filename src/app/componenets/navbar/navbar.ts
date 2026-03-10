c726443b10aa98150716c42676ec071d0226e2c7
Pree14 <preethi98942@gmail.com>
2026-03-10 12:50:23 +0530

    Changes updated

--- src/app/componenets/navbar/navbar.ts
+++ src/app/componenets/navbar/navbar.ts
@@ -1,15 +1,39 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [RouterLink, RouterLinkActive],
+  imports: [RouterLink, RouterLinkActive, CommonModule],
   templateUrl: './navbar.html',
   styleUrl: './navbar.scss',
 })
-export class Navbar {
+export class Navbar implements OnInit{
+  isDark = false;
+
+  ngOnInit() {
+    const savedTheme = localStorage.getItem('theme');
+
+    if (savedTheme === 'dark') {
+      document.body.classList.add('dark-theme');
+      this.isDark = true;
+    }
+  }
+
   pdfDownload(){
     window.open('assets/Preethi_AngularDeveloper_wrong.pdf', '_blank');
+  }
+
+  toggleTheme() {
+    this.isDark = !this.isDark;
+
+    if (this.isDark) {
+      document.body.classList.add('dark-theme');
+      localStorage.setItem('theme', 'dark');
+    } else {
+      document.body.classList.remove('dark-theme');
+      localStorage.setItem('theme', 'light');
+    }
   }
 }
